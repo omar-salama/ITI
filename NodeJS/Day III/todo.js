5989c87b95904fa0158a472b4a49188d8d391157
Omar Salama <omarzalama@gmail.com>
2021-06-15 06:25:25 +0200

    linking app with mongodb using mongoose

--- NodeJS/Day III/todo.js
+++ NodeJS/Day III/todo.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = new express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.listen(3000);
+
+const url =
+  "mongodb+srv://osalama:OSalama_123@initialcluster.uen29.mongodb.net/Todo?retryWrites=true&w=majority";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Cat = mongoose.model("Cat", { name: String });
+
+const kitty = new Cat({ name: "Zildjian" });
+kitty.save().then(() => console.log("meow"));
