7d28537eabf9b5fe893b9e89ee9f17e787821a1b
omar-salama <31420520+omar-salama@users.noreply.github.com>
2021-06-16 03:22:41 +0200

    Delete db_connection.js

--- NodeJS/Day III/db_connection.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-const url =
-  "mongodb+srv://osalama:OSalama_123@initialcluster.uen29.mongodb.net/Todo?retryWrites=true&w=majority";
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
