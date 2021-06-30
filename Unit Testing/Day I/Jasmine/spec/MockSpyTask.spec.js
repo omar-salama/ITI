32417a6b9bd6a7271fe135eaa8661dc6950a4ae4
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-06-30 14:43:52 +0200

    Jasmine testing task finished

---
+++ Unit Testing/Day I/Jasmine/spec/MockSpyTask.spec.js
@@ -0,0 +1,33 @@
+var counter = {
+  currentValues: function () {
+    return 1;
+  },
+};
+
+function sumOfValues() {
+  return counter.currentValues();
+}
+
+describe("mock and spy", function () {
+  // spy on counter object currentValue property and mock its implementation to equal [100]
+  // expect some of values is returning the right value (new mocked value 100) after mocking
+  // and expect that we called the spy function for one time
+  it("spying", function () {
+    spyOn(counter, "currentValues").and.returnValue(100);
+    expect(counter.currentValues()).toEqual(100);
+    expect(counter.currentValues).toHaveBeenCalled();
+  });
+
+  // mock a function that takes any string and return number, call it twice and expect that it is called twice
+  it("mocking", function () {
+    const string_caller = jasmine
+      .createSpy("string caller")
+      .and.callFake(function (string) {
+        return string.length;
+      });
+
+    expect(string_caller("omar")).toEqual(4);
+    expect(string_caller("salama")).toEqual(6);
+    expect(string_caller).toHaveBeenCalledTimes(2);
+  });
+});
